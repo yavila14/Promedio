de678a4f4ab637cbf5b5779502bd5c5036390f85
Yeimy <yei14@hotmail.com>
2021-01-05 21:20:11 -0500

    Se modifico el programa con el uso del while y el switch

--- mainPromedioWhile.cpp
+++ mainPromedioWhile.cpp
@@ -3,13 +3,11 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int n,i;
+	int i,op;
 	float promedio,nota1,nota2,nota3;
-	i=1;
-	n=0;
-	cout <<"Digite numero de estudiantes para calcular promedio  ";
-	cin >>n;
-	while (i<=n){
+	i=0;
+while(i<=1){
+	cout <<"Por favor ingrese datos del estudiante para obtener su promedio \n";
 	cout <<"Ingrese nota 1 ";
 	cin >>nota1;
 	cout <<"Ingrese nota 2 ";
@@ -17,9 +15,17 @@ int main(int argc, char** argv) {
 	cout <<"Ingrese nota 3 ";
 	cin >>nota3;
 	promedio=(nota1+nota2+nota3)/3;
-	cout <<"El promedio del estudiante  " <<i <<" es  " <<promedio <<endl;
-	i=i+1;
+	cout <<"El promedio del estudiante es  " <<promedio <<endl <<endl;
+	cout <<"Desea ingresar otro estudiante  1=si / 0=no "<<endl;
+	cin >>op;
+	switch(op){
+		case 0:i=2;
+		break;
+		case 1:i=1;
+		break;
+		default:i=3;
+		cout <<"Opcion incorrecta ";
+	}
 }
-	
 	return 0;
 }
